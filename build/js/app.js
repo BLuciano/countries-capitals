a930dbcb9ee8068117734860e577e77172ee6df4
Luciano Bruzzoni <lucianobruzzoni@yahoo.com>
2016-05-23 02:27:33 -0700

    Added loading animation for data request

--- build/js/app.js
+++ build/js/app.js
@@ -14,14 +14,17 @@ mainApp.config(['$routeProvider', function($routeProvider){
 		controller : "countriesCtrl"
 	});
 }])
-.controller("countriesCtrl", ['$scope', 'getData', 'showCountry', 
-	function($scope, getData, showCountry){
+.controller("countriesCtrl", ['$scope', 'getData', 'showCountry', '$rootScope',
+'$timeout', function($scope, getData, showCountry, $rootScope, $timeout){
+	$rootScope.loading = true;
 	
 	getData()
 	.then(function(data){
 		$scope.countries = data.geonames;
+		$timeout(function() {
+        	$rootScope.loading = false;
+      	}, 1000);
 	});
-
 	$scope.showCountry = showCountry;
 }]);
 mainApp.config(['$routeProvider', function($routeProvider){
